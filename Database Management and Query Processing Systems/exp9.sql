3c3d3164e942613a37c98707b60a8aadee7265b6
Atharv Dayanand Shet Govekar <atharvgovekar777@gmail.com>
2025-11-07 07:52:02 +0530

    ..

--- Database Management and Query Processing Systems/exp9.sql
+++ Database Management and Query Processing Systems/exp9.sql
@@ -5,6 +5,7 @@ CREATE USER 'emp3'@'localhost' IDENTIFIED BY 'emp3W';
 
 GRANT ALL PRIVILEGES ON Atharv.view1 TO 'emp1'@'localhost';
 
+
  GRANT SELECT ON Atharv.employee TO 'emp2'@'localhost';
 GRANT UPDATE (Empname, deptno) ON Atharv.employee TO 'emp2'@'localhost' WITH GRANT OPTION;
 
@@ -17,24 +18,23 @@ GRANT UPDATE (Empname, deptno) ON Atharv.employee TO 'emp1'@'localhost';
 EXIT;
 
 mysql -u root -p
-FLUSH PRIVILEGES;
-
+GRANT ALL PRIVILEGES ON *.* TO 'emp3'@'localhost';
+EXIT;
 
 SHOW GRANTS FOR 'emp1'@'localhost';
 SHOW GRANTS FOR 'emp2'@'localhost';
 SHOW GRANTS FOR 'emp3'@'localhost';
 
-FLUSH PRIVILEGES;
 
-mysql -u root -p
-FLUSH PRIVILEGES;
+REVOKE ALL PRIVILEGES ON Atharv.view1 FROM 'emp1'@'localhost';
 
 
-CREATE INDEX idx_empname ON employee (Empname);
+SHOW GRANTS FOR 'empl'@'localhost';
+
+
+CREATE INDEX idx ON employee (Empname);
 
 SELECT user, host FROM mysql.user WHERE user IN ('emp1', 'emp2', 'emp3');
-
-
 SHOW GRANTS FOR 'emp1'@'localhost';
 SHOW GRANTS FOR 'emp2'@'localhost';
 SHOW GRANTS FOR 'emp3'@'localhost';
